7c37166237aeb6026c4c152a92c27b32aeda937a
Roman Vavtunkevuch <roman.vavrun@gmail.com>
2019-02-18 12:38:55 +0200

    initial variable & getCardOptions function in progres

---
+++ FLX_homework_10/homework/src/credits_handler.js
@@ -0,0 +1,13 @@
+function userCard (index) {
+    let balance = 100;
+    let transactionLimit = 100;
+    let historyLogs = [];
+    let key = index;
+    
+    function getCardOptions() {
+        console.log('Balance: ' + balance + '\n',
+        + 'transactionLimit: ' + transactionLimit + '\n',
+        + 'historyLogs: ' + historyLogs + '\n',
+        + 'key: ' + key + '\n')
+    }
+}
