ccb0981e82ee595c29025323ad079f767ba14d60
Roman Vavtunkevuch <roman.vavrun@gmail.com>
2019-02-19 13:52:55 +0200

    fn takeCredits ready

--- FLX_homework_10/homework/src/credits_handler.js
+++ FLX_homework_10/homework/src/credits_handler.js
@@ -21,4 +21,19 @@ function userCard (index) {
         });
     }
 
+    function takeCredits(value) {
+        if (value <= balance && value <= transactionLimit) {
+            balance -= value;
+            historyLogs.push({
+                operationType: 'Withdrawal of credits',
+                credits: value,
+                operationTime: new Date().toLocaleString('en-GB')
+            });
+        } else if (value > balance) {
+            console.log('You don\'t have enough money, current balance is: ' + balance);
+        } else {
+            console.log('You surpassed transaction limit, current limit is: ' + transactionLimit);
+        }
+    }
+
 }
